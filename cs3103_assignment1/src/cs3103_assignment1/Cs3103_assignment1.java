3a6686558d1523f0592a8805d7edf170e37444a5
clement <clementlow@outlook.com>
2014-11-13 01:09:17 +0800

    a

---
+++ cs3103_assignment1/src/cs3103_assignment1/Cs3103_assignment1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs3103_assignment1;
+
+/**
+ *
+ * @author Clement
+ */
+public class Cs3103_assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
